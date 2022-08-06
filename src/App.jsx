0880d3aea0a98c076a072e8df6d31b1ab22ca7bf
Manu Morante <manu@estadologico.com>
2022-08-06 22:49:03 +0200

    renames

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Container from './components/Container'
-import Projects from './components/Projects/List'
-import Social from './components/Social/List'
-import UserCard from './components/UserCard'
+import Projects from './components/projects2/List'
+import Social from './components/social2/List'
+import Card from './components/user/Card'
 
 export default function App() {
   return (
     <div className='grid grid-cols-1 md:grid-cols-[240px_auto] items-start'>
       <div className='p-10 bg-white h-full'>
         <div className='md:fixed'>
-          <UserCard />
+          <Card />
           <Social />
         </div>
       </div>
--- src/components/user/Card.jsx
+++ src/components/user/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Avatar from './Avatar'
 
-export default function UserCard() {
+export default function Card() {
   return (
     <div className='flex flex-col gap-3 items-center md:items-start'>
       <Avatar />
       <div className='w-full'>
-        <div className='text-center md:text-left text-xl font-semibold'>
+        <div className='text-center md:text-left text-2xl font-normal text-neutral-800'>
           Manu Morante
         </div>
-        <div className='text-center md:text-left'>
+        <div className='text-center text-2xl font-light md:text-left text-neutral-400'>
           Frontend developer
           {/* and UI designer */}
         </div>
