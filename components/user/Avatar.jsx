6150ef6ffd4cbea04e797356d3ee285ae94e4059
Manu Morante <manu@estadologico.com>
2022-08-07 03:00:04 +0200

    Projects covers as OG

--- components/user/Avatar.jsx
+++ components/user/Avatar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Avatar() {
-  const avatarUrl =
-    'https://secure.gravatar.com/avatar/3d6f5d17df19913a7a7970923563710e?s=256'
+  const avatarUrl = 'https://secure.gravatar.com/avatar/3d6f5d17df19913a7a7970923563710e?s=256'
 
   return (
-    <img
+    <Image
+      src={avatarUrl}
+      width={128}
+      height={128}
       className='block w-32 h-auto rounded-2xl overflow-hidden'
       alt='Manu Morante'
-      src={avatarUrl}
     />
   )
 }
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['secure.gravatar.com'],
+  },
   experimental: {
     images: {
       allowFutureImage: true,
