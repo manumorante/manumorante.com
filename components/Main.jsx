26c4cf6bd1e9d44cfcf82cc64e8b5967bf75c1a3
Manu Morante <manu@estadologico.com>
2022-08-07 04:37:49 +0200

    Waves

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import UserCard from './user/Card'
 import SocialList from './social/List'
+import cx from 'classnames'
 
 export default function Main() {
   return (
-    <div className='[Main] h-full bg-white flex flex-col justify-between p-10'>
+    <div
+      className={cx(
+        '[Main] h-full max-h-screen',
+        'flex flex-col justify-between p-10',
+        'bg-white bg-[url("/waves.svg")] bg-[length:100%] bg-bottom bg-no-repeat'
+      )}>
       <UserCard />
       <SocialList />
     </div>
--- components/social/Item.jsx
+++ components/social/Item.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Item({ icon, label, href }) {
   return (
     <a href={href} target='blank'>
-      <div className='flex flex-col items-center gap-1 py-3 font-medium text-neutral-400 md:hover:text-neutral-600 transition-all'>
+      <div className='flex flex-col md:flex-row items-center gap-1 py-3 font-medium text-neutral-400 md:hover:text-neutral-600 transition-all'>
         <div className='w-6 h-6'>{icon}</div>
-        <span className='text-sm font-light overflow-hidden'>{label}</span>
+        <span className='text-sm md:text-base font-light overflow-hidden'>{label}</span>
       </div>
     </a>
   )
