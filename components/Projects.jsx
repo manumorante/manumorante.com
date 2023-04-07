010745508ed0e086fcbe5b837618a4305f09b10e
Manu <manu@estadologico.com>
2023-04-07 16:40:15 +0200

    Move projects info here

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,15 +1,7 @@
-import { meta } from 'config'
-
+import { projects } from 'projects.js'
 import Project from './Project'
 
-async function getProjects() {
-  const res = await fetch(meta.projects.url)
-  if (!res.ok) throw new Error('Failed to fetch data')
-  return res.json()
-}
-
 export default async function Projects() {
-  const { projects } = await getProjects()
   const published = projects.filter((project) => project.published)
 
   if (published?.length <= 0) return null
