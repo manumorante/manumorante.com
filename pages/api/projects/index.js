10c67798017fe1b0299debe263f7a0663a0162ff
Manu Morante <manu@estadologico.com>
2023-05-20 22:26:55 +0200

    API response json with projects name

--- pages/api/projects/index.js
+++ pages/api/projects/index.js
@@ -1,5 +1,5 @@
 import { projects } from 'projects.json'
 export default function handler(req, res) {
   const published = projects.filter((project) => project.published)
-  res.status(200).json(published)
+  res.status(200).json({ projects: published })
 }
