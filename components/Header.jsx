ffa5baa02a1983239fcd1bc4b94d602f7db4a5e3
Manu Morante <manu@estadologico.com>
2023-07-01 22:12:15 +0200

    wave parallax with framer motion

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,24 @@
+'use client'
+
+import { motion, useScroll, useTransform, useSpring } from 'framer-motion'
 import cx from 'clsx'
 import Image from 'next/image'
+import { useRef } from 'react'
 
 export default function Header() {
-  const mainCx = cx('Header relative w-full min-h-screen overflow-hidden')
-  const containerCx = cx('max-w-3xl mx-4 mm:mx-6 sm:mx-auto pt-[20vh]')
+  const ref = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['start start', 'end start'],
+  })
+  const parallax = useTransform(scrollYProgress, [0, 1], [400, -400])
+
+  const mainCx = cx('Header relative min-w-screen min-h-screen overflow-hidden')
+  const containerCx = cx('max-w-3xl mx-4 mm:mx-6 lg:mx-auto pt-[20vh]')
   const hiCx = cx('Hi w-72 sm:w-96 text-8xl font-black leading-none')
 
   return (
-    <div className={mainCx}>
+    <div className={mainCx} ref={ref}>
       <div className={containerCx}>
         <div className={hiCx}>
           <span className='text-neutral-500'>Hi,</span> <span>you are great</span>{' '}
@@ -15,22 +26,27 @@ export default function Header() {
         </div>
 
         <div className='w-72 sm:w-96 my-[30vh] text-6xl font-extralight'>
-          <div>Take a look while you are here.</div>
+          Take a look while you are here.
         </div>
       </div>
 
-      <Image
-        className={cx(
-          'Background', //
-          'block w-full min-w-[756px] h-[352px]',
-          'absolute top-[40vh] left-0 right-0 -z-10',
-          'object-center'
-        )}
-        src='/wave.png'
-        width={1512}
-        height={704}
-        alt='Background'
-      />
+      <motion.div
+        className='absolute top-0 left-0 w-full h-[352px] min-w-[756px] overflow-hidden'
+        //
+        style={{ y: parallax }}
+        //
+      >
+        <Image
+          className={cx(
+            'Background', //
+            'w-full h-full'
+          )}
+          src='/wave.png'
+          width={1512}
+          height={704}
+          alt='Background'
+        />
+      </motion.div>
     </div>
   )
 }
