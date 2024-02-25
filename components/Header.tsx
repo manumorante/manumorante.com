e80728a260f7d2fcc26f8bbd8fe33900ae86b1d6
Manu Morante <manu@estadologico.com>
2024-02-25 02:06:43 +0100

    header animation

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,27 +5,22 @@ import cx from "clsx"
 
 export default function Header() {
   const mainCx = cx("Header w-full h-screen py-10 sm:py-16")
-
+  const textCx = cx("Text container relative z-10")
   const waveCx = cx("Wave", "absolute z-0 top-1/2 left-0", "w-full", "h-1/2")
 
   // Texts
   const hiCx = cx(
-    "Hi",
     "w-0 sm:w-2/3", // force one line
     "text-5xl mm:text-7xl font-black leading-none",
     // Light
     "text-neutral-800 [&_strong]:text-neutral-500",
     "dark:text-neutral-500 dark:[&_strong]:text-white"
   )
-  const lookCx = cx(
-    "Look",
-    "pt-20 sm:py-24",
-    "text-4xl mm:text-5xl font-extralight"
-  )
+  const lookCx = cx("pt-20 sm:py-24", "text-4xl mm:text-5xl font-extralight")
 
   return (
     <div className={mainCx}>
-      <div className="container relative z-10">
+      <div className={textCx}>
         <p className={hiCx}>
           Hi, <strong>you are great</strong>
         </p>
