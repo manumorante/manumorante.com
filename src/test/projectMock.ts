5ae7e3caa1400b5a57398ff1ef87a2cbe76a663c
Manu Morante <manu@estadologico.com>
2025-06-01 02:31:11 +0200

    feat: add mock project data for testing purposes

---
+++ src/test/projectMock.ts
@@ -0,0 +1,10 @@
+export const mockProject = {
+  id: 'test',
+  name: 'Test Project',
+  image: { light: 'light.jpg' },
+  description: 'Test Description',
+  tags: ['React'],
+  url: 'https://test.com',
+  repository: 'https://github.com/test',
+  color: { light: '#fff', dark: '#000' },
+}
