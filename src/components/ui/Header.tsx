1836abda98dc4e9e3b06b53c8bb7cf275093f6fe
Manu Morante <manu@estadologico.com>
2025-06-07 00:34:15 +0200

    refactor: improve Header component layout and styling

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,22 +1,22 @@
 export default function Header() {
   return (
     <div className="Header w-full py-10 sm:py-16">
-      <div className="container mx-auto relative z-10">
+      <div className="relative z-10 container mx-auto">
         <div className="space-y-8">
           <img
-            className="Avatar block w-24 h-24 rounded-full"
-            src="https://secure.gravatar.com/avatar/3d6f5d17df19913a7a7970923563710e?s=256"
+            className="Avatar block h-36 w-36 rounded-full"
+            src="https://www.gravatar.com/avatar/9ffd40e78c60ff6c30e110354ee28d360328ff5b5b16948f314363c24095da4e?size=288&d=initials"
             width={128}
             height={128}
             loading="eager"
             alt="Manu Morante"
           />
-          <p className="text-4xl tracking-tight text-neutral-800 dark:text-neutral-200 font-extrabold leading-none">
+          <p className="text-4xl leading-none font-extrabold tracking-tight text-neutral-800 dark:text-neutral-200">
             Manu Morante <br />
-            <strong className="text-neutral-500 font-extrabold">Frontend developer</strong>
+            <strong className="font-extrabold text-neutral-500">Frontend developer</strong>
           </p>
 
-          <p className="text-2xl dark:text-neutral-200 font-light max-w-lg">
+          <p className="max-w-lg text-2xl font-light dark:text-neutral-200">
             Extensive experience in both product-based companies, focusing on scalable components,
             and consulting firms, where speed and reusability have been prioritized.
           </p>
