08a290b0beb5928a4f1593fa49d8f3f14b615664
Manu Morante <manu@estadologico.com>
2025-06-07 01:16:40 +0200

    chore: add React Router configuration and update Vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 /// <reference types="vitest" />
 import path from 'path'
+import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [reactRouter(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
