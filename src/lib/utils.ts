045e734ec8cfb88ae0b45281de6a5ea2a99058f4
Manu Morante <manu@estadologico.com>
2025-06-07 03:32:19 +0200

    refactor: move formatDuration function to utils.ts and remove duplicate implementation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,26 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDuration(start: Date, end: Date): string {
+  let years: number = end.getFullYear() - start.getFullYear()
+  let months: number = end.getMonth() - start.getMonth()
+
+  // Ajustar si los meses son negativos
+  if (months < 0) {
+    years--
+    months += 12
+  }
+
+  // Formatear la duración
+  let duration: string = ''
+  if (years > 0 && months > 0) {
+    duration = `${years} year${years !== 1 ? 's' : ''}, ${months} month${months !== 1 ? 's' : ''}`
+  } else if (years > 0) {
+    duration = `${years} year${years !== 1 ? 's' : ''}`
+  } else if (months > 0) {
+    duration = `${months} month${months !== 1 ? 's' : ''}`
+  }
+
+  return duration
+}
