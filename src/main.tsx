d083f24782ec7c1586a8eff7967ea5d030b40db8
Manu Morante <manu@estadologico.com>
2025-07-06 00:42:26 +0200

    move index to styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import AppRouter from './router'
 
 createRoot(document.getElementById('root')!).render(
