3d9a2a63946af0ef504089b89322caa379ffd5af
Manu Morante <manu@estadologico.com>
2025-07-06 03:25:24 +0200

    refactor: update Header component to use Avatar component and adjust layout styles

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,19 +1,12 @@
-import { avatar } from '@/data/config'
+import { Avatar } from '@/components/ui'
 import { cn } from '@/lib/cn'
 
 export default function Header() {
   return (
-    <div className="Header w-full py-10 sm:py-16">
-      <div className="relative z-10 container mx-auto">
+    <div className="Header w-full py-14 sm:py-16">
+      <div className="relative z-10 container">
         <div className="space-y-8">
-          <img
-            className="Avatar block h-36 w-36 rounded-full"
-            src={avatar.medium}
-            width={144}
-            height={144}
-            loading="eager"
-            alt="Manu Morante"
-          />
+          <Avatar />
           <p
             className={cn(
               'text-dark dark:text-lightest',
@@ -21,12 +14,12 @@ export default function Header() {
               'leading-none font-bold tracking-tight',
             )}>
             Manu Morante{' '}
-            <strong className="text-accent dark:text-primary block font-light">
+            <strong className="text-primary dark:text-primary block font-light">
               Frontend developer
             </strong>
           </p>
 
-          <p className="max-w-lg text-2xl font-light">
+          <p className="max-w-lg text-xl font-light sm:text-2xl">
             Extensive experience in both product-based companies, focusing on scalable components,
             and consulting firms, where speed and reusability have been prioritized.
           </p>
