51278fe62cd910d600d80d735e13fda0b4d84dc6
Manu Morante <manu@estadologico.com>
2025-07-06 03:23:32 +0200

    refactor: replace main tag with fragment in HomePage component for improved structure

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,10 +3,10 @@ import { Footer, Header } from '@/components/ui'
 
 export default function Home() {
   return (
-    <main className="px-6">
+    <>
       <Header />
       <Projects />
       <Footer />
-    </main>
+    </>
   )
 }
